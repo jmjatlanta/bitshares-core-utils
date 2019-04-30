1fd850e529290046c1130b5e733cb3b8cc611a70
John Jones <jmjatlanta@gmail.com>
2019-04-30 07:28:56 -0500

    Add build tag script

---
+++ build_tag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=$1
+DEV_DIR=/home/jmjatlanta/Development/cpp
+
+cd $DEV_DIR
+git clone ssh://git@github.com/bitshares/bitshares-core bitshares-core-$VERSION
+cd bitshares-core-$VERSION
+git checkout tags/$VERSION
+git submodule update --init --recursive
+cmake -DCMAKE_BUILD_TYPE=Release .
+make -j8
