f0bbf5fc1a023d03ba0ea748f94cfab2f0dd5ddf
John Jones <jmjatlanta@gmail.com>
2019-05-15 13:25:03 -0500

    windows updates

--- build_branch.bat
+++ build_branch.bat
@@ -1,8 +1,16 @@
 @echo off
 
+set DEV_DIR=c:\Development\cpp
+set BRANCH=%1
 SET VS_VERSION="Visual Studio 15 2017"
 SET BOOST_DIR="c:\Development\cpp\boost169vs2017"
 SET CURL_DIR="c:\Program Files (x86)\CURL"
 
+cd %DEV_DIR%
+git clone https://github.com/bitshares/bitshares-core bitshares-core-%BRANCH%
+cd bitshares-core-%BRANCH%
+git checkout %BRANCH%
+git submodule update --init --recursive
 cmake -G %VS_VERSION% -A x64 -DBOOST_ROOT=%BOOST_DIR% -DCURL_STATICLIB=ON -DCURL_LIBRARY=%CURL_DIR%\lib\libcurl_imp.lib -DCURL_INCLUDE_DIR=%CURL_DIR%\include -DOPENSSL_CONF_SOURCE="C:\Program Files\Common Files\SSL\openssl.cnf"
-cmake --build . --target install --config Release -j 2
+cmake --build . --target install --config Release -j 3
+
--- build_tag.bat
+++ build_tag.bat
@@ -9,9 +9,11 @@ IF "%1%"=="" (
 IF "%2"=="" (
   SET VS_VERSION="Visual Studio 16 2019"
   SET BOOST_DIR="c:\Development\cpp\boost170"
+  SET CURL_DIR="c:\Program Files (x86)\CURL"
 ) ELSE (
   SET VS_VERSION="Visual Studio 15 2017"
-  SET BOOST_DIR="c:\Development\cpp\boost170vs2017"
+  SET BOOST_DIR="c:\Development\cpp\boost169vs2017"
+  SET CURL_DIR="c:\Program Files (x86)\CURL"
 )
 
 c:
@@ -21,7 +23,7 @@ git clone https://github.com/bitshares/bitshares-core bitshares-core-%VERSION%
 cd bitshares-core-%VERSION%
 git checkout tags/%VERSION%
 git submodule update --init --recursive
-cmake -G %VS_VERSION% -A x64 -DBOOST_ROOT=%BOOST_DIR% -DCURL_STATICLIB=ON -DCURL_LIBRARY="C:\Program Files\CURL\lib\libcurl_imp.lib" -DCURL_INCLUDE_DIR="C:\Program Files\CURL\include" -DOPENSSL_CONF_SOURCE="C:\Program Files\Common Files\SSL\openssl.cnf"
+cmake -G %VS_VERSION% -A x64 -DBOOST_ROOT=%BOOST_DIR% -DCURL_STATICLIB=ON -DCURL_LIBRARY=%CURL_DIR%\lib\libcurl_imp.lib -DCURL_INCLUDE_DIR=%CURL_DIR%\include -DOPENSSL_CONF_SOURCE="C:\Program Files\Common Files\SSL\openssl.cnf"
 cmake --build . --target install --config Release
 
 :Exit
